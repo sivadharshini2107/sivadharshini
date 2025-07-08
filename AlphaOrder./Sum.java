9e7b294d816821cf86c76757c78bf7114e6042e0
sivadharshini2107 <sharewithsivadharshini@gmail.com>
2025-07-08 18:01:45 +0530

    Sum.java

---
+++ AlphaOrder./Sum.java
@@ -0,0 +1,10 @@
+mport java.util.Scanner;
+public class Sum
+{
+    public static void main(String[] args){
+    Scanner s=new Scanner(System.in);
+    int n1=s.nextInt();
+    int n2=s.nextInt();
+    System.out.println("The sum of "+n1+" and "+n2+" is "+(n1+n2));
+    }
+}
