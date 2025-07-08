ca7923ba65951111a94f2e3430320f13390d35ca
sivadharshini2107 <sharewithsivadharshini@gmail.com>
2025-07-08 17:55:34 +0530

    AcceptString.java

---
+++ AcceptString.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class AcceptString {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        String s1=s.next();
+        String s2=s.next();
+        System.out.println(s1+" Technologies "+ s2);
+    }
+}
