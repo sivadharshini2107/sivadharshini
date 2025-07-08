297885d470c9a2665c7a448dd02238348c89ed4c
sivadharshini2107 <sharewithsivadharshini@gmail.com>
2025-07-08 18:01:19 +0530

    OddEven.java

---
+++ AlphaOrder./OddEven.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class OddEven
+{
+    public static void main(String[] args){
+    Scanner s=new Scanner(System.in);
+    int n1=s.nextInt();
+    if(n1%2==0)
+    System.out.println("Even Number");
+    else
+    System.out.println("odd Number");
+    }
+}
