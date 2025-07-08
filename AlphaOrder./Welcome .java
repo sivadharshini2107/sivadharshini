52244e33b8fa440016ebee6170a870f42bd96be8
sivadharshini2107 <sharewithsivadharshini@gmail.com>
2025-07-08 18:02:47 +0530

    Welcome .java

---
+++ AlphaOrder./Welcome .java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Welcome {
+    public static void main(String[] args) {
+    Scanner s=new Scanner(System.in);
+    String s1=s.next();
+    System.out.println("Welcome "+s1);
+    }
+}
